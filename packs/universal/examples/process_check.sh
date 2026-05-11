733b84e4c90f525567f7666b4023c80184b91eca
vilaspatel <42242176+vilaspatel@users.noreply.github.com>
2026-05-11 17:21:48 +0530

    Update process_check.sh

--- packs/universal/examples/process_check.sh
+++ packs/universal/examples/process_check.sh
@@ -1,5 +1,16 @@
 #!/usr/bin/env bash
 set -euo pipefail
 
-ps -eo pid,ppid,user,%cpu,%mem,cmd --sort=-%cpu
-exit 0
+ps -eo pid,ppid,user,%cpu,%mem,cmd --sort=-%cpu \
+| awk 'BEGIN {
+  print "["
+}
+NR>1 {
+  printf "  {\"pid\":%s,\"ppid\":%s,\"user\":\"%s\",\"cpu\":%s,\"mem\":%s,\"cmd\":\"",
+         $1,$2,$3,$4,$5;
+  for (i=6;i<=NF;i++) printf "%s ", $i;
+  print "\"},"
+}
+END {
+  print "]"
+}'
